240c38cfc6744e9e520d8bcbed6e3f94fb950665
Manan Bhatia <mananbhatia02@gmail.com>
2024-05-02 00:42:44 +0530

    added logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
-
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 export default function Home() {
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/logout");
+            router.replace("/login");
+        } catch (error) {
+            console.log("Error in logout", error);
+        }
+    };
+
     return (
         <div>
             <h1>hello</h1>
+            <Button onClick={logout}>Logout</Button>
         </div>
     );
 }
