a4146153af04a27a8bbe5daa18cc28acb6863f00
Manan Bhatia <mananbhatia02@gmail.com>
2024-05-05 21:15:47 +0530

    removed /analytics route was pushed by mistake

--- src/app/api/admin/analytics/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { connect } from "@/helpers/dbConfig";
-connect();
-import User from "@/models/userModel";
-
-export async function GET(request: NextRequest) {}
